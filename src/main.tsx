85156bdea62c6b7d69b3f3869d779148b4c4db30
Carlos Eduardo <carloseduardolbraz@gmail.com>
2022-06-24 12:36:32 -0300

    configurando apollo com variáveis de ambiente

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter } from 'react-router-dom'
+
 import App from './App'
+
+import { client } from './lib/apollo';
+
 import './styles/global.css'
-
-
-const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4r0wwcu0g7301z35zrhgpvy/master',
-  cache: new InMemoryCache()
-});
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
